038ad0ee85b796566457c522c0d1e16b62c51cc5
Ashank Mishra <ashankkumarmishra@gmail.com>
2021-01-16 20:13:53 +0530

    Empty String is passed in arguments

--- src/main/java/com/main/project/Calculator.java
+++ src/main/java/com/main/project/Calculator.java
@@ -2,6 +2,6 @@ package com.main.project;
 
 public class Calculator {
 	public int Add(String numbers) {
-		return 0;
+		return 0; // Returns zero for an empty String
 	}
 }
--- src/test/java/org/main/test/TestCalculator.java
+++ src/test/java/org/main/test/TestCalculator.java
@@ -9,7 +9,7 @@ public class TestCalculator {
 	@Test
 	public void addString() {
 		Calculator calculator = new Calculator();
-		String numbersToBeAdded = "";
+		String numbersToBeAdded = ""; // Empty String
 		int sum = calculator.Add(numbersToBeAdded);
 		Assert.assertEquals(0, sum);
 	}
