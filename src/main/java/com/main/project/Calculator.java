fcf5f2b503eb53c98c9afa2269875fc4cea7d793
Ashank Mishra <ashankkumarmishra@gmail.com>
2021-01-17 20:13:43 +0530

    Support for different delimiters added

--- src/main/java/com/main/project/Calculator.java
+++ src/main/java/com/main/project/Calculator.java
@@ -1,21 +1,55 @@
 package com.main.project;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Calculator {
 	public int Add(String numbers) {
 		if (numbers != null && !numbers.equals("")) {
-			String commaSeparatedString = numbers.replaceAll("\\n", ","); // This will replace new line with delimiter
-			String[] numbersArray = commaSeparatedString.split(","); // Split the String and convert it into array
-			int sum = addNumbers(numbersArray);
-			return sum;
+			Map<String, Object> stringAndDelimiter = getStringAndDelimiter(numbers);
+			if (stringAndDelimiter != null && stringAndDelimiter.get("number") != null) {
+				String commaSeparatedString = stringAndDelimiter.get("number").toString().replaceAll("\\n",
+						stringAndDelimiter.get("delimiter").toString());
+				// This will replace new line with delimiter
+				String[] numbersArray = commaSeparatedString.split(stringAndDelimiter.get("delimiter").toString());
+				// Split the String and convert it into array
+				int sum = addNumbers(numbersArray);
+				return sum;
+			} else {
+				return 0;
+			}
+
 		} else {
 			return 0; // Returns zero for an empty String
+		}
+	}
+
+	private Map<String, Object> getStringAndDelimiter(String numbers) {
+		try {
+			Map<String, Object> mapStringAndDelimiter = new HashMap<String, Object>();
+			String possibleDelimiterIndicator = numbers.substring(0, 2);
+			if (possibleDelimiterIndicator.equals("//")) {
+				String delimiter = numbers.substring(2, 3);
+				String number = numbers.substring(3);
+				mapStringAndDelimiter.put("delimiter", delimiter);
+				mapStringAndDelimiter.put("number", number);
+			} else {
+				mapStringAndDelimiter.put("delimiter", ",");
+				mapStringAndDelimiter.put("number", numbers);
+			}
+			return mapStringAndDelimiter;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 
 	private int addNumbers(String[] numbersArray) {
 		int sum = 0;
 		for (String number : numbersArray) {
-			sum += Integer.parseInt(number);
+			if (number != null && !number.equals("")) {
+				sum += Integer.parseInt(number);
+			}
 		}
 		return sum;
 	}
--- src/test/java/org/main/test/TestCalculator.java
+++ src/test/java/org/main/test/TestCalculator.java
@@ -1,16 +1,30 @@
 package org.main.test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.main.project.Calculator;
 
 public class TestCalculator {
+	private Calculator calculator;
+
+	@Before
+	public void setup() {
+		calculator = new Calculator();
+	}
+
 	@Test
 	public void addString() {
-		Calculator calculator = new Calculator();
-		String numbersToBeAdded = "4\n2,56"; // Multiple numebrs to be added
+		String numbersToBeAdded = "4\n2,56"; // Multiple numbers to be added
 		int sum = calculator.Add(numbersToBeAdded);
 		Assert.assertEquals(62, sum);
+	}
+
+	@Test
+	public void addMultipleNumbersInStringWithDifferentDelimiters() {
+		String numbersWithDelimiters = "//;\n1;2;5\n4"; // String of numbers with varying delimiters
+		int sum = calculator.Add(numbersWithDelimiters);
+		Assert.assertEquals(12, sum);
 	}
 }
